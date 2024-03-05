b151b3b880cf563d6817a3c379d358e1e4f59da6
Chang Liu <liuc223@gmail.com>
2024-03-05 20:44:47 +0800

    Remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,18 +48,7 @@ const config = {
           // editUrl:
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-          blogSidebarCount: "ALL",
-          blogSidebarTitle: "All our posts",
-          postsPerPage: 5,
-        },
+        blog: false,
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -86,7 +75,7 @@ const config = {
             position: "left",
             label: "Docs",
           },
-          { to: "/blog/", label: "Blog", position: "left" },
+          { href: "//blog.kuzudb.com", label: "Blog", position: "left" },
           {
             type: "html",
             position: "right",
