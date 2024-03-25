244175a835e13112ee00d1320ff4db8519716723
prrao87 <prrao87@gmail.com>
2024-03-25 17:30:23 -0400

    Add Astro files

---
+++ README.md
@@ -0,0 +1,63 @@
+# Kùzu Documentation
+
+[![Built with Starlight](https://astro.badg.es/v2/built-with-starlight/tiny.svg)](https://starlight.astro.build)
+
+This documentation site is built with [Astro](https://astro.build) and
+[Starlight](https://starlight.astro.build). The sections below describe the site's structure and
+the commands for installation, local development and build instructions.
+
+## 🚀 Project Structure
+
+Inside of this Astro + Starlight project, there exist the following folders and files:
+
+```
+.
+├── public/
+├── src/
+│   ├── assets/
+│   ├── content/
+│   │   ├── docs/
+│   │   └── config.ts
+│   └── env.d.ts
+├── astro.config.mjs
+├── package.json
+└── tsconfig.json
+```
+
+Starlight looks for `.md` or `.mdx` files in the `src/content/docs/` directory. Each file is exposed
+as a route based on its file name.
+
+> [!NOTE]
+> MDX combines JSX and Markdown to make it easier to write component-driven content like tabbed code blocks.
+> Starlight requires that you work with `.mdx` files instead of `.md` when using JSX components. See the official
+> [documentation](https://mdxjs.com/) for more information on MDX syntax and why it exists.
+
+Images are added to `src/assets/` and embedded in Markdown with a relative link.
+
+Any additional static assets (like fonts, favicon, PDFs, etc.) that will not be processed by Astro
+are placed in the `public/` directory.
+
+## 🧞 Commands
+
+All commands are run from the root of the project, from a terminal:
+
+| Command                   | Action                                           |
+| :------------------------ | :----------------------------------------------- |
+| `npm install`             | Installs dependencies                            |
+| `npm run dev`             | Starts local dev server at `localhost:4321`      |
+| `npm start`               | Starts local server and builds site              |
+| `npm run build`           | Build your production site to `./dist/`          |
+| `npm run preview`         | Preview your build locally, before deploying     |
+| `npm run astro ...`       | Run CLI commands like `astro add`, `astro check` |
+| `npm run astro -- --help` | Get help using the Astro CLI                     |
+
+## 👀 Learn more
+
+Check out [Starlight’s docs](https://starlight.astro.build/) and read
+[the Astro documentation](https://docs.astro.build) for further customization of the site.
+
+## Deployment
+
+A CI pipeline is configured to deploy the documentation to the server. The pipeline is triggered
+when a commit is pushed to the main branch. The CI pipeline will automatically build the
+documentation and deploy it to https://docs.kuzudb.com.
---
+++ astro.config.mjs
@@ -0,0 +1,85 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            // site: 'https://docs.kuzudb.com',
+            favicon: '/src/assets/favicon.ico',
+            title: 'Kùzu',
+            logo: {
+                light: '/src/assets/logo/kuzu-logo.png',
+                dark: '/src/assets/logo/kuzu-logo-inverse.png',
+                replacesTitle: true,
+            },
+            social: {
+                github: 'https://github.com/withastro/starlight',
+                discord: 'https://discord.gg/jw7xN2ZhJB',
+                twitter: 'https://twitter.com/kuzudb',
+                linkedin: 'https://www.linkedin.com/company/101059770',
+                youtube: 'https://youtube.com/@kuzudb',
+            },
+            customCss: ['./src/styles/custom.css'],
+            expressiveCode: true,
+            sidebar: [
+                {
+                    label: 'Home',
+                    link: '/',
+                },
+                {
+                    label: 'Installation',
+                    link: '/installation/',
+                },
+                {
+                    label: 'Quick start',
+                    link: '/quick-start/',
+                },
+                {
+                    label: 'RDFGraphs',
+                    badge: 'New',
+                    collapsed: true,
+                    items: [
+                        { label: 'Motivation', link: '/rdf-graphs' },
+                        { label: 'RDF basics', link: '/rdf-graphs/rdf-basics' },
+                        { label: 'Example RDFGraph', link: '/rdf-graphs/example-rdfgraph' },
+                        { label: 'Overview & Cypher clauses', link: '/rdf-graphs/rdfgraphs-overview' },
+                        { label: 'RDF data import', link: '/rdf-graphs/rdf-import' },
+                        { label: 'Preloaded RDFGraphs', link: '/rdf-graphs/rdfgraphs-repo' },
+                    ],
+                    autogenerate: { directory: 'reference' },
+                },
+                {
+                    label: 'Cypher manual',
+                    collapsed: false,
+                    items: [
+                        { label: 'Overview', link: '/cypher'},
+                        { label: 'Data types', link: '/cypher/data-types'},
+                        { label: 'Query clauses', link: '/cypher/query-clauses' },
+                        { label: 'Functions, expressions & operators', link: '/cypher/expressions' },
+                        { label: 'Data definition', link: '/cypher/data-definition' },
+                        { label: 'Data manipulation', link: '/cypher/data-manipulation-clauses' },
+                        { label: 'Scan', link: '/cypher/load_from' },
+                        { label: 'Copy', link: '/cypher/copy' },
+                    ],
+                    autogenerate: { directory: 'reference' },
+                },
+                {
+                    label: 'Blog 🔗',
+                    link: 'https://blog.kuzudb.com',
+                    attrs: { target: '_blank' },
+                },
+            ],
+            plugins: process.env.CHECK_LINKS
+                ? [
+                        starlightLinksValidator({
+                            errorOnFallbackPages: true,
+                            errorOnInconsistentLocale: true,
+                        }),
+                  ]
+                : [],
+        }),
+    ],
+});
+
