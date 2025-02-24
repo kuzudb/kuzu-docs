a9f05229197178cae70cbc9986bdc76c6f2f6338
Guodong Jin <guod.jin@gmail.com>
2025-02-25 04:03:11 +0800

    Release 0.8.2 (#385)
    * Add GCS docs (#374)
    
    * Add GCS docs
    
    * Self-review
    
    * Update doc for export db default format (#377)
    
    * Update doc for export db default format
    
    * Update src/content/docs/migrate/index.md
    
    ---------
    
    Co-authored-by: Prashanth Rao <35005448+prrao87@users.noreply.github.com>
    
    * Update doc for customized stopwords list (#376)
    
    * Update doc for customized stopwords list
    
    * Update full-text-search.md
    
    * Update full-text-search.md
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Prashanth Rao <35005448+prrao87@users.noreply.github.com>
    
    * Add doc for unity catalog (#378)
    
    * Add doc for unity catalog
    
    * Update unity.md
    
    * Cleanup and reorg for publish
    - Move iceberg and delta to external data sources section
    - Add new methods to the index page
    - Update sidebar
    
    ---------
    
    Co-authored-by: prrao87 <prrao87@gmail.com>
    
    * Fix headers and cleanup httpfs extension docs (#380)
    
    * fix: link (#382)
    
    * Add docs for GCS globbing (#381)
    
    * Add docs for GCS glob
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Prashanth Rao <35005448+prrao87@users.noreply.github.com>
    
    * Normalize example dataset names (#383)
    
    * Normalize example dataset names
    
    * Fix
    
    * Update src/content/docs/extensions/httpfs.md
    
    * Update src/content/docs/extensions/full-text-search.md
    
    * Update full-text-search.md (#386)
    
    * Update full-text-search.md
    
    * Update full-text-search.md
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Prashanth Rao <35005448+prrao87@users.noreply.github.com>
    
    * Make GCS credential management docs more consistent with S3 docs formatting-wise
    
    * Add sidebar pages
    
    * Bump versions
    
    * Address comment
    
    * Update S3 server API requirements (#387)
    
    * Improve CLI getting started docs (#388)
    
    - Closes #384
    
    ---------
    
    Co-authored-by: Royi Luo <r.luo@hotmail.ca>
    Co-authored-by: ziyi chen <chenziyi990424@gmail.com>
    Co-authored-by: Prashanth Rao <35005448+prrao87@users.noreply.github.com>
    Co-authored-by: prrao87 <prrao87@gmail.com>
    Co-authored-by: Siddhartha Sahu <867424+sdht0@users.noreply.github.com>
    Co-authored-by: Royi Luo <r34luo@uwaterloo.ca>

--- astro.config.mjs
+++ astro.config.mjs
@@ -95,7 +95,7 @@ export default defineConfig({
                         { label: 'Overview', link: '/export' },
                         { label: 'Copy to CSV', link: '/export/csv' },
                         { label: 'Copy to Parquet', link: '/export/parquet' },
-                        { label: 'Copy to JSON', link: '/export/json', badge: { text: 'New'}},
+                        { label: 'Copy to JSON', link: '/export/json'},
                     ]
                 },
                 {
@@ -189,18 +189,27 @@ export default defineConfig({
                     collapsed: true,
                     items: [
                         { label: 'Overview', link: '/extensions'},
-                        { label: 'HTTP File System (httpfs)', link: '/extensions/httpfs'},
+                        { 
+                            label: 'HTTP File System (httpfs)',
+                            collapsed: true,
+                            items: [
+                                { label: 'HTTPS', link: '/extensions/httpfs#https-file-system'},
+                                { label: 'Amazon S3', link: '/extensions/httpfs#aws-s3-file-system'},
+                                { label: 'Google Cloud Storage', link: '/extensions/httpfs#gcs-file-system', badge: { text: 'New' }},
+                            ]
+                        },
                         {
-                            label: 'External databases',
+                            label: 'External data sources',
                             items: [
                                 { label: 'Overview', link: '/extensions/attach' },
                                 { label: 'External Kùzu databases', link: '/extensions/attach/kuzu' },
                                 { label: 'External relational databases ', link: '/extensions/attach/rdbms' },
+                                { label: 'Iceberg', link: '/extensions/iceberg' },
+                                { label: 'Delta Lake', link: '/extensions/delta' },
+                                { label: 'Unity Catalog', link: '/extensions/attach/unity', badge: { text: 'New' }},
                             ]
                         },
                         { label: 'JSON', link: '/extensions/json' },
-                        { label: 'Iceberg', link: '/extensions/iceberg' },
-                        { label: 'Delta Lake', link: '/extensions/delta' },
                         { label: 'Full-text search', link: '/extensions/full-text-search', badge: { text: 'New' }},
                     ],
                     autogenerate: { directory: 'reference' },
