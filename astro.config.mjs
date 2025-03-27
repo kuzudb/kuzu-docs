ed7a5ccccd58cf93be822fa59570cde79ba6419e
prrao87 <prrao87@gmail.com>
2025-03-27 15:36:33 -0400

    Reorg RDBMS extensions into their own pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -242,7 +242,15 @@ export default defineConfig({
                             items: [
                                 { label: 'Overview', link: '/extensions/attach' },
                                 { label: 'External Kuzu databases', link: '/extensions/attach/kuzu' },
-                                { label: 'External relational databases ', link: '/extensions/attach/rdbms' },
+                                {
+                                    label: 'External relational databases ',
+                                    collapsed: true,
+                                    items: [
+                                        { label: 'PostgreSQL', link: '/extensions/attach/postgres' },
+                                        { label: 'DuckDB', link: '/extensions/attach/duckdb' },
+                                        { label: 'SQLite', link: '/extensions/attach/sqlite' },
+                                    ]
+                                },
                                 { label: 'Iceberg', link: '/extensions/iceberg' },
                                 { label: 'Delta Lake', link: '/extensions/delta' },
                                 { label: 'Unity Catalog', link: '/extensions/attach/unity', badge: { text: 'New' }},
